2c230b201cd0854ea6405683f2f0c098281f0309
Dialectica <jc@thedebatecompany.com>
2016-03-03 12:06:45 -0800

    layout keyed to firebase , intentionality factory

--- app/chat/chat.js
+++ app/chat/chat.js
@@ -10,10 +10,21 @@
           $scope.messages.$add({text: newMessage});
         }
       };
+
+      $scope.addIntentionality = function(srcMessage, destMessage, type) {
+        if (srcMessage && destMessage && type) {
+          $scope.intentionality.$add({source: srcMessage.$id, destination: destMessage.$id, type: type})
+        }
+      };
     }]);
 
   app.factory('messageList', ['fbutil', '$firebaseArray', function(fbutil, $firebaseArray) {
     var ref = fbutil.ref('messages');
+    return $firebaseArray(ref);
+  }]);
+
+  app.factory('intentionality', ['fbutil', '$firebaseArray', function(fbutil, $firebaseArray) {
+    var ref = fbutil.ref('intentionality');
     return $firebaseArray(ref);
   }]);
 
@@ -40,16 +51,51 @@
           .attr("height", h);
 
       var force = d3.layout.force()
-          .charge(-120)
+          .charge(-100)
           .linkDistance(80)
-          .size([w, h]);
+          .size([w, h])
+          .alpha(0.1)
+          .start();
+
+      //Now we are giving the SVGs co-ordinates - the force layout is generating the co-ordinates which this code is using to update the attributes of the SVG elements
+      force.on("tick", function () {
+        /*link.attr("x1", function (d) {
+         return d.source.x;
+         })
+         .attr("y1", function (d) {
+         return d.source.y;
+         })
+         .attr("x2", function (d) {
+         return d.target.x;
+         })
+         .attr("y2", function (d) {
+         return d.target.y;
+         });
+         */
+
+        d3.selectAll("circle").attr("cx", function (d) {
+              return d.x;
+            })
+            .attr("cy", function (d) {
+              return d.y;
+            });
+
+        d3.selectAll("text").attr("x", function (d) {
+              return d.x;
+            })
+            .attr("y", function (d) {
+              return d.y;
+            });
+
+        force.start();
+
+      });
 
 // watch the last 10 caused this to fire repeatedly and seize up the sim
       scope.$watch('messages', function(messages){
 
         force.nodes(messages)
             //.links(graph.links)
-            .start();
 
 //Create all the line svgs but without locations yet
         /*        var link = svg.selectAll(".link")
@@ -60,9 +106,8 @@
          return Math.sqrt(d.value);
          });
          */
-//Do the same with the circles for the nodes
         var node = svg.selectAll(".node")
-            .data(messages)
+            .data(messages, function(d) { return d.$id; })
             .enter().append("g")
             .attr("class", "node")
             .call(force.drag);
@@ -76,38 +121,6 @@
             .attr("dy", ".35em")
             .attr('stroke', 'black')
             .text(function(d) { return d.text });
-
-//Now we are giving the SVGs co-ordinates - the force layout is generating the co-ordinates which this code is using to update the attributes of the SVG elements
-        force.on("tick", function () {
-          /*link.attr("x1", function (d) {
-           return d.source.x;
-           })
-           .attr("y1", function (d) {
-           return d.source.y;
-           })
-           .attr("x2", function (d) {
-           return d.target.x;
-           })
-           .attr("y2", function (d) {
-           return d.target.y;
-           });
-           */
-
-          d3.selectAll("circle").attr("cx", function (d) {
-                return d.x;
-              })
-              .attr("cy", function (d) {
-                return d.y;
-              });
-
-          d3.selectAll("text").attr("x", function (d) {
-                return d.x;
-              })
-              .attr("y", function (d) {
-                return d.y;
-              });
-
-        });
 
       }, true);
 
